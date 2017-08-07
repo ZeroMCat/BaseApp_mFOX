489199cfa32b5c097eaec8ec7d953148afa83db8
MaxZero_FOX <516260191@qq.com>
2017-08-07 14:53:39 +0800

    测试分支提交

--- app/src/main/java/com/baseapp/mfox/baseapp_mfox/MainActivity.java
+++ app/src/main/java/com/baseapp/mfox/baseapp_mfox/MainActivity.java
@@ -1,13 +1,22 @@
 package com.baseapp.mfox.baseapp_mfox;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.TV_ALL)
+    TextView mTVALL;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        mTVALL.setText("你好，我的APP");
     }
 }
